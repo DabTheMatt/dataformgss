6a60e765afd22db95c5d72505336cb3e90798a36
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-09-26 21:09:54 +0200

    Updates

---
+++ precache-manifest.bbb114c5b1f40d00f34fb42fdea0b1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b37726af11a5e79a9e02edd8998f616",
+    "url": "/dataformgss/index.html"
+  },
+  {
+    "revision": "ab366fc3d38eca9edbfa",
+    "url": "/dataformgss/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e399fa3be6812abf0b1",
+    "url": "/dataformgss/static/js/2.fb84c3b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dataformgss/static/js/2.fb84c3b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab366fc3d38eca9edbfa",
+    "url": "/dataformgss/static/js/main.53081220.chunk.js"
+  },
+  {
+    "revision": "013d7f5fce9e516c8a89",
+    "url": "/dataformgss/static/js/runtime-main.aaa7295e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dataformgss/static/media/logo.5d5d9eef.svg"
+  }
+]);
